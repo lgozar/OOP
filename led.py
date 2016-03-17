d2779bc66876012cfee68a51c933a075f1ae72d5
lgozar <lgozar@salisbury6c.ac.uk>
2016-03-17 15:43:21 +0000

    Update led.py

--- led.py
+++ led.py
@@ -1,3 +1,25 @@
-import board.py
+from board import Board
+from time import sleep
 
 class LED:
+
+    def __init__(self, gpio, pin):
+        self.board = gpio
+        self.pin = pin
+        
+    def setup_led(self):
+        self.board.GPIO.setup(self.pin, self.board.GPIO.OUT)
+        
+    def turnLED_on(self):
+        self.board.GPIO.output(self.pin, self.board.GPIO.HIGH)
+        
+    def turnLED_off(self):
+        self.board.GPIO.output(self.pin, self.board.GPIO.LOW)
+        
+if __name__ = "__main__"
+
+    rpi = Board()
+    led = LED(rpi, pin)
+    led.turnLED_on()
+    sleep(0.1)
+    led.turnLED_off()
