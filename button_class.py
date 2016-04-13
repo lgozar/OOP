c7f1d85e6db635f2c35f367e2c49e6b4c845268d
lgozar <lgozar@salisbury6c.ac.uk>
2016-04-13 12:49:31 +0100

    Update button_class.py

--- button_class.py
+++ button_class.py
@@ -6,15 +6,6 @@ class Button:
         self.__board = gpio_object
         self.pin = pin
         self.button()
-        self.pressed = False
-        
-    @property
-    def pressed(self):
-        return self.__pressed
-
-    @pressed.setter
-    def pressed(self, value):
-        self.__pressed = value
         
     def button(self):
         self.__board.GPIO.setup(self.pin, self.__board.GPIO.IN, pull_up_down = self.__board.GPIO.PUD_UP)
@@ -25,8 +16,7 @@ if __name__ == "__main__":
     button = Button(rpi, 24)
     
     while True:
-        if button.pressed:
+        if button == False:
             print('Button Pressed')
-            button.pressed = False
             time.sleep(0.2)
     rpi.GPIO.cleanup()
