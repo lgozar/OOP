7615e23ee69949161641a8f026bb535ed9369d26
lgozar <lgozar@salisbury6c.ac.uk>
2016-04-14 15:15:53 +0100

    Update and rename led.py to rgb.py

---
+++ rgb.py
@@ -0,0 +1,46 @@
+from board import Board
+from time import sleep
+
+class RGB:
+
+    def __init__(self, gpio_object, r, g, b):
+        self.red = r
+        self.green = g
+        self.blue = b
+        self.__outputs = [self.red, self.green, self.blue]
+        self.board = gpio
+        
+        self.setup_led()
+        
+    def setup_led(self):
+        print('Setting up pins')
+        for pins in self.__outputs:
+            self.board.GPIO.setup(pins, self.board.GPIO.OUT)
+        
+    def turnRed_on(self):
+        self.turnRGB_off()
+        self.board.GPIO.output(self.red, self.board.GPIO.HIGH)
+    
+    def turnBlue_on(self): 
+        self.turnRGB_off()
+        self.board.GPIO.output(self.blue, self.board.GPIO.HIGH)
+        
+    def turnGreen_on(self): 
+        self.turnRGB_off()
+        self.board.GPIO.output(self.green, self.board.GPIO.HIGH)
+        
+    def turnRGB_off(self):
+        for pins in self.__outputs:
+            self.board.GPIO.output(pins, self.board.GPIO.LOW)
+        
+if __name__ == "__main__":
+
+    rpi = Board()
+    rgb = RGB(rpi, 16, 20, 21)
+    rgb.turnRed_on()
+    sleep(0.1)
+    rgb.turnGreen_on()
+    sleep(0.1)
+    rgb.turnBlue_on()
+    sleep(0.1)
+    led.turnRGB_off()
