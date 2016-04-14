44472c6cd83b94d8aab5893e772320ff706cc182
lgozar <lgozar@salisbury6c.ac.uk>
2016-04-14 15:41:25 +0100

    Update button_rgb.py

--- button_rgb.py
+++ button_rgb.py
@@ -8,15 +8,12 @@ button = Button(rpi, 24)
 rgb = RGB(rpi, 16, 20, 21)
 
 while True:
-        if button.pressed:
-            rgb.turnRed_on()
-            sleep(0.1)
-        if button.pressed:    
-            rgb.turnGreen_on()
-            sleep(0.1)
-        if button.pressed: 
-            rgb.turnBlue_on()
-            sleep(0.1)
-        if button.pressed:
-            rgb.turnRGB_off()
+    if button.pressed:
+        rgb.turnRed_on()
+        sleep(0.1)
+        rgb.turnGreen_on()
+        sleep(0.1)
+        rgb.turnBlue_on()
+        sleep(0.1)
+        rgb.turnRGB_off()
 rpi.GPIO.cleanup()
