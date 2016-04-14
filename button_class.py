c52ec41c1c2c882f51731287524b0c2836233425
lgozar <lgozar@salisbury6c.ac.uk>
2016-04-14 14:54:21 +0100

    Update button_class.py

--- button_class.py
+++ button_class.py
@@ -20,11 +20,11 @@ class Button:
 
     def __button(self):
         self.__board.GPIO.setup(self.__pin, self.__board.GPIO.IN, pull_up_down = self.__board.GPIO.PUD_UP)
+        
+        self.__board.GPIO.add_event_detect(self.__pin, self.__board.GPIO.FALLING, callback=self.__my_callback, bouncetime=300)
 
-'''        
     def __my_callback(self, channel):
         self.pressed = True
-''''
 
 if __name__ == "__main__":
   
