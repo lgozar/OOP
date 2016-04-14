0a0d696f267440d6fe076a04763c15c0a900afc4
lgozar <lgozar@salisbury6c.ac.uk>
2016-04-14 15:28:00 +0100

    Create rgb_class.py

---
+++ rgb_class.py
@@ -0,0 +1,34 @@
+from board import Board
+from time import sleep
+
+class RGB:
+
+    def __init__(self, gpio_object, r, g, b):
+        self.red = r
+        self.green = g
+        self.blue = b
+        self.__outputs = [self.red, self.green, self.blue]
+        self.board = gpio_object
+        
+        self.setup_led()
+        
+    def setup_led(self):
+        print('Setting up pins')
+        for pins in self.__outputs:
+            self.board.GPIO.setup(pins, self.board.GPIO.OUT)
+        
+    def turnRed_on(self):
+        self.turnRGB_off()
+        self.board.GPIO.output(self.red, self.board.GPIO.HIGH)
+    
+    def turnBlue_on(self): 
+        self.turnRGB_off()
+        self.board.GPIO.output(self.blue, self.board.GPIO.HIGH)
+        
+    def turnGreen_on(self): 
+        self.turnRGB_off()
+        self.board.GPIO.output(self.green, self.board.GPIO.HIGH)
+        
+    def turnRGB_off(self):
+        for pins in self.__outputs:
+            self.board.GPIO.output(pins, self.board.GPIO.LOW)
