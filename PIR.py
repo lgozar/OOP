19e73e4a10c56e9cf438812c86b8a47317840f7f
lgozar <lgozar@salisbury6c.ac.uk>
2016-04-20 11:22:28 +0100

    Update PIR.py

--- PIR.py
+++ PIR.py
@@ -1,1 +1,19 @@
 from board import Board
+from PIR_class import PIR
+from time import sleep
+
+rpi = Board()
+pir = PIR(rpi, 17)
+
+try:
+    print('PIR Starting')
+    sleep(0.2)
+    print('Ready!')
+    while True:
+        if rpi.GPIO.input(pir):
+            print('Motion Detected')
+        sleep(0.1)
+        
+except KeyboardInterrupt:
+    print('Quit')
+    rpi.GPIO.cleanup()
