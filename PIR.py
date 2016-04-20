247fe0d9ca0ec193c62aa16b709d9e73c5a892b9
lgozar <lgozar@salisbury6c.ac.uk>
2016-04-20 11:09:40 +0100

    Update PIR.py

--- PIR.py
+++ PIR.py
@@ -2,4 +2,10 @@ from board import Board
 
 class PIR:
     
-    def __init__(self, gpio_object, pir):
+    def __init__(self, gpio_object, pin):
+        self.__board = gpio_object
+        self.__pin = pin
+        
+    def setup_pir():
+        self.__board.GPIO.setup(self.pin, self.__board.GPIO.IN)
+        
