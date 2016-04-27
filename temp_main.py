86469885fd3314706309dd8eb14d1bf430af9a90
lgozar <lgozar@salisbury6c.ac.uk>
2016-04-27 11:59:16 +0100

    Update temp_main.py

--- temp_main.py
+++ temp_main.py
@@ -44,15 +44,15 @@ def set_temp(current):
         if button1.pressed:
             print('Button 1 pressed')
             button2.pressed = False
-            temp += num
+            value = temp += num
             button1.pressed = False
         elif button2.pressed:
             print('Button 2 pressed')
             button1.pressed = False
-            temp -= num
+            value = temp -= num
             button2.pressed = False
             
-    return temp
+    return value
 
 try:
     
