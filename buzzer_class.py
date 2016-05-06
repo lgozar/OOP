a718a80d477190a65352e0dfede3ab26aa683f0a
lgozar <lgozar@salisbury6c.ac.uk>
2016-05-06 09:45:46 +0100

    Update buzzer_class.py

--- buzzer_class.py
+++ buzzer_class.py
@@ -1,1 +1,58 @@
+from board import Board
+import time
 
+class MorseCode:
+
+    def ___init__(self, gpio_object, pin):
+        self.__board = gpio_object
+        self.__pin = pin
+        self.__setup()
+        self.__morsecode()
+        
+    def __setup(self):
+        self.__board.GPIO.setup(self.__pin, self.__board.GPIO.OUT)
+        
+    def __morsecode(self):
+    
+        '''dot dot dot'''
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.HIGH)
+        time.sleep(0.1)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.LOW)
+        time.sleep(0.1)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.HIGH)
+        time.sleep(0.1)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.LOW)
+        time.sleep(0.1)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.HIGH)
+        time.sleep(0.1)
+
+        '''dash dash dash'''
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.LOW)
+        time.sleep(0.2)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.HIGH)
+        time.sleep(0.2)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.LOW)
+        time.sleep(0.2)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.HIGH)
+        time.sleep(0.2)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.LOW)
+        time.sleep(0.2)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.HIGH)
+        time.sleep(0.2)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.LOW)
+        time.sleep(0.2)
+    
+        '''dot dot dot'''
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.HIGH)
+        time.sleep(0.1)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.LOW)
+        time.sleep(0.1)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.HIGH)
+        time.sleep(0.1)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.LOW)
+        time.sleep(0.1)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.HIGH)
+        time.sleep(0.1)
+    
+        
+        
