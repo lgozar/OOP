fa0d4abf7b995dfd9db23caf8631cbf0c3de7a90
lgozar <lgozar@salisbury6c.ac.uk>
2016-05-09 11:35:26 +0100

    Create buzzer_class.py

---
+++ buzzer_class.py
@@ -0,0 +1,54 @@
+from board import Board
+import time
+
+class Buzzer:
+
+    def ___init__(self, gpio_object, pin):
+        self.__board = gpio_object
+        self.__pin = pin
+        self.__setup()
+        self.__main()
+        
+    def __setup(self):
+        self.__board.GPIO.setup(self.__pin, self.__board.GPIO.OUT)
+        
+    def __main(self):
+    
+        '''dot dot dot'''
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.HIGH)
+        time.sleep(0.1)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.LOW)
+        time.sleep(0.1)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.HIGH)
+        time.sleep(0.1)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.LOW)
+        time.sleep(0.1)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.HIGH)
+        time.sleep(0.1)
+
+        '''dash dash dash'''
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.LOW)
+        time.sleep(0.2)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.HIGH)
+        time.sleep(0.2)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.LOW)
+        time.sleep(0.2)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.HIGH)
+        time.sleep(0.2)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.LOW)
+        time.sleep(0.2)
+    
+        '''dot dot dot'''
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.HIGH)
+        time.sleep(0.1)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.LOW)
+        time.sleep(0.1)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.HIGH)
+        time.sleep(0.1)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.LOW)
+        time.sleep(0.1)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.HIGH)
+        time.sleep(0.1)
+    
+        
+        
