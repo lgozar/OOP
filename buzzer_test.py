e842c7d60bc4b8b41f2e94278f0af437c2f83466
lgozar <lgozar@salisbury6c.ac.uk>
2016-05-09 12:52:37 +0100

    Update buzzer_test.py

--- buzzer_test.py
+++ buzzer_test.py
@@ -4,50 +4,50 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(pin, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
 
 count = 0
 
 def morsecode():
     
     '''dot dot dot'''
-    GPIO.output(pin, GPIO.HIGH)
+    GPIO.output(26, GPIO.HIGH)
     time.sleep(0.1)
-    GPIO.output(pin, GPIO.LOW)
+    GPIO.output(26, GPIO.LOW)
     time.sleep(0.1)
-    GPIO.output(pin, GPIO.HIGH)
+    GPIO.output(26, GPIO.HIGH)
     time.sleep(0.1)
-    GPIO.output(pin, GPIO.LOW)
+    GPIO.output(26, GPIO.LOW)
     time.sleep(0.1)
-    GPIO.output(pin, GPIO.HIGH)
+    GPIO.output(26, GPIO.HIGH)
     time.sleep(0.1)
 
     '''dash dash dash'''
-    GPIO.output(pin, GPIO.LOW)
+    GPIO.output(26, GPIO.LOW)
     time.sleep(0.2)
-    GPIO.output(pin, GPIO.HIGH)
+    GPIO.output(26, GPIO.HIGH)
     time.sleep(0.2)
-    GPIO.output(pin, GPIO.LOW)
+    GPIO.output(26, GPIO.LOW)
     time.sleep(0.2)
-    GPIO.output(pin, GPIO.HIGH)
+    GPIO.output(26, GPIO.HIGH)
     time.sleep(0.2)
-    GPIO.output(pin, GPIO.LOW)
+    GPIO.output(26, GPIO.LOW)
     time.sleep(0.2)
-    GPIO.output(pin, GPIO.HIGH)
+    GPIO.output(26, GPIO.HIGH)
     time.sleep(0.2)
-    GPIO.output(pin, GPIO.LOW)
+    GPIO.output(26, GPIO.LOW)
     time.sleep(0.2)
     
     '''dot dot dot'''
-    GPIO.output(pin, GPIO.HIGH)
+    GPIO.output(26, GPIO.HIGH)
     time.sleep(0.1)
-    GPIO.output(pin, GPIO.LOW)
+    GPIO.output(26, GPIO.LOW)
     time.sleep(0.1)
-    GPIO.output(pin, GPIO.HIGH)
+    GPIO.output(26, GPIO.HIGH)
     time.sleep(0.1)
-    GPIO.output(pin, GPIO.LOW)
+    GPIO.output(26, GPIO.LOW)
     time.sleep(0.1)
-    GPIO.output(pin, GPIO.HIGH)
+    GPIO.output(26, GPIO.HIGH)
     time.sleep(0.1)
     
 os.system('clear')
