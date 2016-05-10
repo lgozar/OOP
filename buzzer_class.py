9a44e57fe1f2fc4341b33ea854b797d3b37833f1
lgozar <lgozar@salisbury6c.ac.uk>
2016-05-10 09:02:52 +0100

    Update buzzer_class.py

--- buzzer_class.py
+++ buzzer_class.py
@@ -7,12 +7,11 @@ class Buzzer:
         self.__board = gpio_object
         self.__pin = pin
         self.__setup()
-        self.__main()
         
     def __setup(self):
         self.__board.GPIO.setup(self.__pin, self.__board.GPIO.OUT)
         
-    def __main(self):
+    def __SOS(self):
     
         '''dot dot dot'''
         self.__board.GPIO.output(self.__pin, self.__board.GPIO.HIGH)
@@ -49,6 +48,13 @@ class Buzzer:
         time.sleep(0.1)
         self.__board.GPIO.output(self.__pin, self.__board.GPIO.HIGH)
         time.sleep(0.1)
-    
+        
+    def __beep(self):
+        
+        '''beep'''
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.HIGH)
+        time.sleep(0.1)
+        self.__board.GPIO.output(self.__pin, self.__board.GPIO.LOW)
+        time.sleep(0.1)
         
         
