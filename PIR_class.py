c88dd693a1a3a1e7ed7f92846a283fe4937ee28b
lgozar <lgozar@salisbury6c.ac.uk>
2016-05-10 10:06:03 +0100

    Update PIR_class.py

--- PIR_class.py
+++ PIR_class.py
@@ -7,14 +7,6 @@ class PIR:
         self.__setup_pir()
         self.__pir_pin = self.__pin
         
-    @property
-    def pir_pin(self):
-        return self.__pir_pin
-        
-    @pir_pin.setter    
-    def pir_pin(self, value):
-        self.__pir_pin = value
-        
     def __setup_pir(self):
         self.__board.GPIO.setup(self.__pin, self.__board.GPIO.IN)
         
