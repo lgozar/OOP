3211e3d01636f548b26952133406fa6312bb2f8b
lgozar <lgozar@salisbury6c.ac.uk>
2016-05-10 10:26:46 +0100

    Update PIR_main.py

--- PIR_main.py
+++ PIR_main.py
@@ -5,14 +5,4 @@ from time import sleep
 rpi = Board()
 pir = PIR(rpi, 17)
 
-try:
-    print('PIR Starting')
-    sleep(0.2)
-    print('Ready!')
-    while True:
-        pir.input_pir()
-        sleep(0.1)
-        
-except KeyboardInterrupt:
-    print('Quit')
-    pir.stop()
+pir.main()
