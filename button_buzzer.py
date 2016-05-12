cee9cf00cb9381f7657b5208d78241ea6cda5905
lgozar <lgozar@salisbury6c.ac.uk>
2016-05-12 14:10:53 +0100

    Update button_buzzer.py

--- button_buzzer.py
+++ button_buzzer.py
@@ -1,8 +1,21 @@
 from button_class import Board
 from buzzer_class import Buzzer
+from time import sleep
 
 rpi = Board()
 button = BUtton(rpi, 24)
 buzz = Buzzer(rpi, 11)
 
 try:
+    while True:
+        if button.pressed:
+            buzz.beep()
+            buzz.stop()
+            button.pressed = False
+            
+except KeyboardInterrupt:
+    buzz.stop()
+    sleep(0.1)
+    
+finally:
+    buzz.stop()
