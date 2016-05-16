5df92bb5a3beb616fd241010c5dbd207073eb311
lgozar <lgozar@salisbury6c.ac.uk>
2016-05-16 11:30:32 +0100

    Update temp_class.py

--- temp_class.py
+++ temp_class.py
@@ -1,1 +1,37 @@
+import os
+import time
 
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+sensor = '/sys/bus/w1/devices/28-021565382dff/w1_slave'
+
+class Temperature:
+
+    def __init__(self):
+        pass
+      
+    @property
+    def c(self):
+        return self.__read_temp()[0]
+        
+    def __temp_raw(self):
+        f = open(sensor, 'r')
+        lines = f.readlines()
+        f.close()
+        return lines
+        
+    def __read_temp(self):
+        lines = self.__temp_raw()
+        while lines[0].strip()[-3:] != 'YES':
+            time.sleep(0.2)
+            lines = self.__temp_raw()
+        output = lines[1].find('t=')
+        if output != -1:
+            string = lines[1].strip()[output+2:]
+            c = float(string) / 1000.0
+            return c
+            
+if __name__ == '__main__':
+    temp_sensor = Temperature()
+    print('Temperature = ' + str(int(temp_sensor.c)) + ' C')
