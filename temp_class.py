e56dfdf749126f0e9dc1d64568ad9551edfdf710
lgozar <lgozar@salisbury6c.ac.uk>
2016-05-16 12:04:23 +0100

    Update temp_class.py

--- temp_class.py
+++ temp_class.py
@@ -13,7 +13,7 @@ class Temperature:
       
     @property
     def c(self):
-        return self.__read_temp()[0]
+        return self.read_temp()[0]
         
     def __temp_raw(self):
         f = open(sensor, 'r')
@@ -21,7 +21,7 @@ class Temperature:
         f.close()
         return lines
         
-    def __read_temp(self):
+    def read_temp(self):
         lines = self.__temp_raw()
         while lines[0].strip()[-3:] != 'YES':
             time.sleep(0.2)
@@ -34,4 +34,4 @@ class Temperature:
             
 if __name__ == '__main__':
     temp_sensor = Temperature()
-    print('Temperature = ' + str(int(temp_sensor.c)) + ' C')
+    print('Temperature = ' + str(temp_sensor.c) + ' C')
