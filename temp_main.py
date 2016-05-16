21238afea9ad9919928e6f95534e3f5e5618c0b6
lgozar <lgozar@salisbury6c.ac.uk>
2016-05-16 11:35:18 +0100

    Update temp_main.py

--- temp_main.py
+++ temp_main.py
@@ -1,35 +1,13 @@
-import os
-import time
 import RPi.GPIO as GPIO
 from board import Board
+from temp_class import Temperature
 from button_class import Button
 from rgb_class import RGB
-
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
-
-sensor = '/sys/bus/w1/devices/28-021565382dff/w1_slave'
-
-def temp_raw():
-    f = open(sensor, 'r')
-    lines = f.readlines()
-    f.close()
-    return lines
-
-def read_temp():
-    lines = temp_raw()
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = temp_raw()
-    output = lines[1].find('t=')
-    if output != -1:
-        string = lines[1].strip()[output+2:]
-        c = float(string) / 1000.0
-        return c
 
 '''main code'''
 
 board = Board()
+temp = Temperature()
 rgb = RGB(board, 16, 20, 21)
 button1 = Button(board, 24)
 button2 = Button(board, 12)
@@ -46,15 +24,15 @@ def down():
 try:
     while True:
         
-        c = read_temp()
+        c = temp.read_temp()
         celsius = str(float(round(c, 2)))
         msg = celsius + ' degress C.'
         print(msg)
             
-        if c < set_temp:
+        if temp.c < set_temp:
             rgb.turnRGB_off()
             rgb.turnRed_on()
-        elif c >= set_temp:
+        elif temp.c >= set_temp:
             rgb.turnRGB_off()
             rgb.turnGreen_on()
         else:
