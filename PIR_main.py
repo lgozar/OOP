7f3a68a61ae83865b012305ae3c2547e1f0d09b9
lgozar <lgozar@salisbury6c.ac.uk>
2016-05-18 10:14:14 +0100

    Update PIR_main.py

--- PIR_main.py
+++ PIR_main.py
@@ -1,18 +1,18 @@
-from board import Board
-from PIR_class import PIR
+from board import Board #import Board class 
+from PIR_class import PIR #import PIR class
 from time import sleep
 
-rpi = Board()
-pir = PIR(rpi, 17)
+rpi = Board() #implement Board class to variable rpi
+pir = PIR(rpi, 17) #implement PIR class to variable pir with pin 17
 
 try:
     print('Starting PIR sensor..')
     sleep(0.2)
     print('Ready!')
     while True:
-        pir.main()
+        pir.main() #runs the main property from the PIR class
         sleep(0.1)
         
 except KeyboardInterrupt:
     print('Stop')
-    pir.stop()
+    pir.stop() #clean up pins
