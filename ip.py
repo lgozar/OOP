56d5aaa9e9249eb97cfa76baf78d3b3961b277cb
lgozar <lgozar@salisbury6c.ac.uk>
2016-05-24 09:18:09 +0100

    Update ip.py

--- ip.py
+++ ip.py
@@ -26,7 +26,7 @@ elif ver == 3:
     ip = 'Use Python V2'
 print('DEBUG: IP Address is ' + str(ip))
 
-lcd = LCD()
+lcd = LCD(ver)
 
 lcd.lcd_string("IP address", lcd.LCD_LINE_1)
 lcd.lcd_string(ip, lcd.LCD_LINE_2)
