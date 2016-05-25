3f58363367d2c71df5aa1fff54cceb269d4cae2f
lgozar <lgozar@salisbury6c.ac.uk>
2016-05-25 11:32:10 +0100

    Update PIR_class.py

--- PIR_class.py
+++ PIR_class.py
@@ -12,7 +12,7 @@ class PIR:
         self.__board.GPIO.setup(self.__pin, self.__board.GPIO.IN)
         
     def main(self):
-        if self.__board.GPIO.input(self.__pin):
+        self.__board.GPIO.input(self.__pin):
             
     def stop(self):
         self.__board.GPIO.cleanup()
