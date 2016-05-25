3b2528e50a0ddd44aec063e1f34cb1735d4d58ad
lgozar <lgozar@salisbury6c.ac.uk>
2016-05-25 11:33:34 +0100

    Update PIR_class.py

--- PIR_class.py
+++ PIR_class.py
@@ -12,7 +12,8 @@ class PIR:
         self.__board.GPIO.setup(self.__pin, self.__board.GPIO.IN)
         
     def main(self):
-        self.__board.GPIO.input(self.__pin):
+        if self.__board.GPIO.input(self.__pin):
+            print('Motion Detected')
             
     def stop(self):
         self.__board.GPIO.cleanup()
